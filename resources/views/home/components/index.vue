f027d09c8fa69e7f04a42d826c7ee2928b6f74bd
sakbly <sofiane.akbly@qrcode-protect.com>
2022-05-23 17:59:32 +0200

    Updated: Home page

--- resources/views/home/components/index.vue
+++ resources/views/home/components/index.vue
@@ -1,23 +1,27 @@
 <template>
-  <ssf-container name="home">
-    <h1></h1>
-  </ssf-container>
+    <ssf-container class="full-absolute full-flex bg-color-1 flex-column text-white" name="home">
+
+        <logo class="mb-3"/>
+        <ssf-text class="font-weight-light text-uppercase h4-responsive">Dashboard</ssf-text>
+
+    </ssf-container>
 </template>
 
 <script>
-  import { defineComponent } from "vue";
+    import { defineComponent } from "vue";
+    import Logo                from "@/components/commons/logo";
 
-  export default defineComponent({
-    name      : "home-index",
-    components: {},
-    metaInfo  : {
-      title: 'Accueil',
-    },
+    export default defineComponent({
+        name      : "home-index",
+        components: { Logo },
+        metaInfo  : {
+            title: 'Accueil',
+        },
 
-    data() {
-      return {}
-    }
-  })
+        data() {
+            return {}
+        }
+    })
 </script>
 
 <style lang="scss" scoped>
