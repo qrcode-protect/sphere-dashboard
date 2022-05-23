4b3afbf672c051c5436ea15deda636cc92185309
sakbly <sofiane.akbly@qrcode-protect.com>
2022-05-23 16:55:32 +0200

    Updated: Form load

--- resources/components/commons/loading/form-load.vue
+++ resources/components/commons/loading/form-load.vue
@@ -1,38 +1,62 @@
 <template>
-  <div v-if="load" id="formLoad" class="section--form-loader full-flex">
-    <triple-spinner :thickness="2" :size="100" color1="#212121" color2="#FC741C" color3="#FFFFFF"/>
-  </div>
+    <ssf-container v-if="load" id="formLoad" class="section--form-loader full-flex">
+        <ssf-loader :color-primary="colors.secondary" class="mx-auto" ripple/>
+    </ssf-container>
 </template>
 
 <script>
 
-  import TripleSpinner from '../loader/triple-spinner'
 
-	import { defineComponent } from "vue";
+    import { defineComponent, onMounted, reactive } from "vue";
 
-	export default defineComponent({
-    name: "form-load",
+    export default defineComponent({
+        name: "form-load",
 
-    components: { TripleSpinner },
+        components: {},
 
-    computed: {
-      load() {
-        return this.$store.getters.loading
-      }
-    },
-  })
+        setup() {
+            const colors = reactive({
+                primary  : '#FBAC4C',
+                secondary: '#D0427D',
+                ternary  : '#7EC5C2',
+            })
+
+            onMounted(() => {
+
+                // init colors
+                (() => {
+                    const root = $(':root')
+                    colors.primary = root.css('--color-1')
+                    colors.secondary = root.css('--color-2')
+                    colors.ternary = root.css('--color-4')
+                })()
+
+            })
+
+            return {
+                colors
+            }
+
+        },
+
+        computed: {
+            load() {
+                return this.$store.getters.loading
+            }
+        },
+    })
 </script>
 
 <style scoped>
 
-  #formLoad {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: rgba(0, 0, 0, .3);
-    z-index: 2050;
-  }
+    #formLoad {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, .3);
+        z-index: 2050;
+    }
 
 </style>
