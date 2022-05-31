73986c3e0a6b65feab65ace80a6491e8d54e51f8
sakbly <sofiane.akbly@qrcode-protect.com>
2022-05-31 16:03:15 +0200

    Updated: Build for production

---
+++ dist/static/js/1931-legacy.ac792ce3.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunksphere_dashboard"]=self["webpackChunksphere_dashboard"]||[]).push([[1931,1096,2533],{36972:function(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__);__webpack_exports__["default"]=__webpack_require__(29914)["default"]}}]);
--- dist/static/js/8872.9bb6c9b7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self["webpackChunksphere_dashboard"]=self["webpackChunksphere_dashboard"]||[]).push([[8872,9699],{5614:function(__unused_webpack_module,__webpack_exports__,__webpack_require__){__webpack_require__.r(__webpack_exports__);__webpack_exports__["default"]=__webpack_require__(1023)["default"]}}]);
