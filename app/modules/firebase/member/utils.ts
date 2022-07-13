0be1a7b6e22bfded6b6346842dc737bfe5c0e6f2
sakbly <sofiane.akbly@qrcode-protect.com>
2022-07-13 09:30:15 +0200

    --Updated-- minor

--- app/modules/firebase/member/utils.ts
+++ app/modules/firebase/member/utils.ts
@@ -5,6 +5,5 @@ import { memberRepository } from "@app/modules/firebase/member/member-repository
 export const memberCount = async () => await memberRepository.count()
 export const memberPremiumCount = async () => {
     const test = await memberRepository.countPremium()
-    console.log(test)
     return test;
 }
